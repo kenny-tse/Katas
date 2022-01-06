e9ffd932aacc56ea8ae3dc89ac2bb5895673b7af
Kenny Tse <kennytse91@gmail.com>
2022-01-05 18:40:21 -0800

    added anagram kata

---
+++ anagrams.js
@@ -0,0 +1,61 @@
+// What is an anagram? Well, two words are anagrams of each other if they both contain the same letters. For example:
+
+// 'abba' & 'baab' == true
+
+// 'abba' & 'bbaa' == true
+
+// 'abba' & 'abbba' == false
+
+// 'abba' & 'abca' == false
+// Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
+
+// anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
+
+// anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer']) => ['carer', 'racer']
+
+// anagrams('laser', ['lazing', 'lazy',  'lacer']) => []
+
+function anagrams(word, words) {
+
+
+  let objectOfLetters = {};
+
+  for (const letter of word) {
+    if (!objectOfLetters[letter]) {
+      objectOfLetters[letter] = 1;
+    } else if (objectOfLetters[letter]) {
+      objectOfLetters[letter] += 1;
+    }
+  }
+
+  let arrayToReturn = [];
+
+  for (const singleWord of words) {
+
+    let objectOfLettersCompare = {};
+
+    for (const letter of singleWord) {
+      if (!objectOfLettersCompare[letter]) {
+        objectOfLettersCompare[letter] = 1;
+      } else if (objectOfLettersCompare[letter]) {
+        objectOfLettersCompare[letter] += 1;
+      }
+    }
+
+    let add = true;
+
+    for (const key of Object.keys(objectOfLettersCompare)) {
+      if (objectOfLettersCompare[key] !== objectOfLetters[key]) {
+        add = false;
+      }
+    }
+
+    if (add === true) {
+      arrayToReturn.push(singleWord);
+    }
+  }
+
+  return arrayToReturn;
+}
+
+console.log(anagrams('racer', ['crazer', 'carer', 'racar', 'caers', 'racer'])); //['carer', 'racer']
