be2eba77f0ca953697c67d5b696d9fcd6d232b34
Kenny Tse <kennytse91@gmail.com>
2022-01-11 18:18:35 -0800

    added stripComments kata

---
+++ stripComments.js
@@ -0,0 +1,49 @@
+// Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
+
+// Example:
+
+// Given an input string of:
+
+// apples, pears # and bananas
+// grapes
+// bananas !apples
+// The output expected would be:
+
+// apples, pears
+// grapes
+// bananas
+// The code would be called like so:
+
+// var result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"])
+// result should == "apples, pears\ngrapes\nbananas"
+
+function solution(input, markers) {
+
+  let returnString = "";
+  let tempInput = "";
+  let inputArray = input.split("");
+  let isPut = true;
+
+  for (const letter of inputArray) {
+
+    if (markers.includes(letter)) {
+      returnString += tempInput.trimEnd();
+      tempInput = "";
+      isPut = false;
+    }
+
+    if (letter === "\n") {
+      isPut = true;
+    }
+
+    if (isPut === true) {
+      tempInput += letter;
+    }
+
+  }
+
+  return returnString;
+};
+
+console.log(solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]));
+
