6fec00fd08e836e0dd46dbee7d103e5ddc944191
Kenny Tse <kennytse91@gmail.com>
2022-01-25 18:06:08 -0800

    added lengthOfLastWord kata

---
+++ lengthOfLastWord.js
@@ -0,0 +1,15 @@
+// Given a string s consisting of some words separated by some number of spaces, return the length of the last word in the string.
+
+// A word is a maximal substring consisting of non-space characters only.
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function (s) {
+
+  let arrayOfWords = s.trim().split(" ");
+
+  return arrayOfWords[arrayOfWords.length - 1].length;
+
+};
