f5944383f0638fd78e0aeac8c0cdcd85124bdb83
Kenny Tse <kennytse91@gmail.com>
2022-02-15 19:55:32 -0800

    adjusted solution

--- isValidWalk.js
+++ isValidWalk.js
@@ -2,41 +2,75 @@
 
 // Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
 
-function isValidWalk(walk) {
+const isValidWalk = (walk) => {
 
   if (walk.length !== 10) {
     return false;
   }
 
-  let n = 0;
-  let e = 0;
-  let s = 0;
-  let w = 0;
+  let objectOfDirections = {};
 
   for (const direction of walk) {
-    if (direction === "n") {
-      n++;
-    }
 
-    if (direction === "e") {
-      e++;
-    }
-
-    if (direction === "s") {
-      s++;
-    }
-
-    if (direction === "w") {
-      w++;
+    if (!objectOfDirections[direction]) {
+      objectOfDirections[direction] = 1;
+    } else {
+      objectOfDirections[direction] += 1;
     }
   }
 
-  if (n !== s || e !== w) {
+  if (objectOfDirections["n"] !== objectOfDirections["s"]) {
+    return false;
+  }
+
+  if (objectOfDirections["e"] !== objectOfDirections["w"]) {
     return false;
   }
 
   return true;
-}
+};
+
+
+// function isValidWalk(walk) {
+
+//   if (walk.length !== 10) {
+//     return false;
+//   }
+
+//   let n = 0;
+//   let e = 0;
+//   let s = 0;
+//   let w = 0;
+
+//   for (const direction of walk) {
+//     if (direction === "n") {
+//       n++;
+//     }
+
+//     if (direction === "e") {
+//       e++;
+//     }
+
+//     if (direction === "s") {
+//       s++;
+//     }
+
+//     if (direction === "w") {
+//       w++;
+//     }
+//   }
+
+//   console.log("north", n)
+//   console.log("east", e)
+//   console.log("south", s)
+//   console.log("west", w)
+
+//   if (n !== s || e !== w) {
+//     return false;
+//   }
+
+//   return true;
+// }
 
 console.log(isValidWalk(['n', 's', 'n', 's', 'n', 's', 'n', 's', 'n', 's'])); // true
 console.log(isValidWalk(['w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e', 'w', 'e'])); // false
