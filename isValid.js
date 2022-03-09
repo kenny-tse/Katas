099ccb2165fd4608bbf0644c3ea7a09e2d39c048
Kenny Tse <kennytse91@gmail.com>
2022-03-08 23:41:43 -0800

    added isValid kata

---
+++ isValid.js
@@ -0,0 +1,30 @@
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+
+const isValid = (paraString) => {
+
+  let stackPara = [];
+
+  for (const bracket of paraString) {
+
+    if (bracket === '(' || bracket === '[' || bracket === '{') {
+      stackPara.push(bracket);
+    } else if (bracket === ')' && stackPara.length > 0 && stackPara[stackPara.length - 1] === '(') {
+      stackPara.pop();
+    } else if (bracket === '}' && stackPara.length > 0 && stackPara[stackPara.length - 1] === '{') {
+      stackPara.pop();
+    } else if (bracket === ']' && stackPara.length > 0 && stackPara[stackPara.length - 1] === '[') {
+      stackPara.pop();
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+console.log(isValid("[()()]"))
